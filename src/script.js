2e64874f93c9fa8e11a7448d4b009504d729af5b
pquauke <philipp.quauke@gmail.com>
2024-11-29 17:23:53 +0100

    Add final functions and styling

--- src/script.js
+++ src/script.js
@@ -1,19 +1,10 @@
 // import CSS into JS
 import './style.css'
 import Chart from 'chart.js/auto'
-// import {} from 'date-fns'
+import { format, fromUnixTime } from 'date-fns'
 import { dom } from './modules'
 
-// .days = array of the next 15 days
-// .days[0-14] = object of the day
-// .hours = array of 24h
-// .hours[0-23]
-
-// windspeed = "windspeed" radar-chart [{ winddir: 180, windspeed: 10 }]
-// windgust = "windgust" radar-chart [{ winddir: 180, windgust: 10 }]
-// winddirection = "winddir" (round to 0 / 45 / 90 / ...)
-
-const location = 'Rjukan'
+getWeatherData('Innsbruck')
 
 async function getWeatherData(location) {
   const apiCall = await fetch(
@@ -21,26 +12,209 @@ async function getWeatherData(location) {
     { mode: 'cors' },
   )
   const rawData = await apiCall.json()
-  console.log(rawData)
-  // chart with temperature & precipitation (timebased)
-  const timeData = await rawData.days[0].hours.map((hour) => ({
-    time: parseInt(hour.datetime.split(':')[0]),
-    temp: hour.temp,
-    precip: hour.precip,
+  const dataByHours = await rawData.days.flatMap((day) =>
+    day.hours.map((hour) => ({
+      hours: parseInt(format(fromUnixTime(hour.datetimeEpoch), 'H')),
+      days: format(fromUnixTime(hour.datetimeEpoch), 'EEEE dd.MM'),
+      temp: hour.temp,
+      icon: hour.icon,
+      precip: hour.precip,
+      windDirection: hour.winddir,
+      windSpeed: hour.windspeed,
+      windGust: hour.windgust,
+    })),
+  )
+  const dataByDays = await rawData.days.map((day) => ({
+    dataTemperature: `min ${parseInt(day.tempmin)}° max ${parseInt(day.tempmax)}°`,
+    dataPrecipitation: `total ${day.precip}mm`,
+    dataWind: `max ${parseInt(day.windspeed)}  (${parseInt(day.windgust)}) km/h`,
   }))
-  //   console.log(timeData)
-  plotLineChart(timeData, 'time', 'temp')
-  plotBarChart(timeData, 'time', 'precip')
-  // icons for each hour
-  const iconData = await rawData.days[0].hours.map((hour) => hour.icon)
-  //   console.log(iconData)
-  plotIcons(iconData)
+  document.querySelector('#location').textContent =
+    `Meteogram for ${rawData.address}`
+  plotDates('#dates', dataByHours)
+  plotLineChart('#temperatureGraphDiv', dataByHours, 'hours', 'temp')
+  plotIcons('#icons', dataByHours)
+  plotBarChart('#precipitationGraphDiv', dataByHours, 'hours', 'precip')
+  plotWindDirection('#windDirection', dataByHours)
+  plotLineChart(
+    '#windGraphDiv',
+    dataByHours,
+    'hours',
+    '',
+    'windSpeed',
+    'windGust',
+  )
+  plotText('#dataTemperature', dataByDays, 'dataTemperature')
+  plotText('#dataPrecipitation', dataByDays, 'dataPrecipitation')
+  plotText('#dataWind', dataByDays, 'dataWind')
 }
 
-getWeatherData(location)
+// define absolute value for the meteogram ------------------------------------
 
-function plotBarChart(data, xKey, yKey) {
-  new Chart(document.getElementById('precipitation'), {
+const aspectRatio = 3 / 40
+const actualWidthCanvas = 6000
+const actualHeightCanvas = actualWidthCanvas * aspectRatio
+const scaledWidthCanvas = 2400 // also adjust CSS
+const scaledHeightCanvas = scaledWidthCanvas * aspectRatio
+const width1day = scaledWidthCanvas / 15
+
+// text -----------------------------------------------------------------------
+
+function plotDates(parentElement, data) {
+  document.querySelector(parentElement).innerHTML = ''
+  data
+    .map((item) => item.days)
+    .forEach((item, i) => {
+      if (i % 24 === 0) {
+        dom.addElement(parentElement, 'span', item, {
+          style: `width: ${width1day}px`,
+        })
+      }
+    })
+}
+
+function plotText(parentElement, data, key) {
+  document.querySelector(parentElement).innerHTML = ''
+  data
+    .map((item) => item[key])
+    .forEach((item) => {
+      dom.addElement(parentElement, 'span', item, {
+        style: `width: ${width1day}px`,
+      })
+    })
+}
+
+// icons ----------------------------------------------------------------------
+
+function plotIcons(parentElement, data) {
+  document.querySelector(parentElement).innerHTML = ''
+  data
+    .map((item) => item.icon)
+    .forEach((item, i) => {
+      if (i % 6 === 3) {
+        dom.addElement(parentElement, 'img', '', {
+          src: `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/2nd%20Set%20-%20Color/${item}.svg`,
+          style: `width: ${width1day / 4}px`,
+        })
+      }
+    })
+}
+
+// winddirection --------------------------------------------------------------
+
+function plotWindDirection(parentElement, data) {
+  document.querySelector(parentElement).innerHTML = ''
+  data
+    .map((item) => item.windDirection)
+    .forEach((item, i) => {
+      if (i % 3 === 0) {
+        dom.addElement(parentElement, 'span', 'navigation', {
+          class: 'material-symbols-outlined',
+          style: `transform: rotate(${item}deg); width: ${width1day / 8}px`,
+        })
+      }
+    })
+}
+
+// charts ---------------------------------------------------------------------
+
+const chartOptions = {
+  responsive: false,
+  maintainAspectRatio: true,
+  scales: {
+    y: {
+      grid: {
+        color: 'rgba(47, 79, 79, 0.5)',
+        lineWidth: (context) => (context.tick.value === 0 ? 4 : 2),
+      },
+      ticks: {
+        display: false,
+      },
+    },
+    x: {
+      ticks: { display: false },
+      grid: { color: 'rgba(47, 79, 79, 0.5)', lineWidth: 4 },
+      afterBuildTicks: function (scale) {
+        scale.ticks = scale.ticks.filter(
+          (tick, index) => index % 24 === 0 && index !== 0,
+        )
+      },
+    },
+  },
+  plugins: {
+    legend: { display: false },
+    ticks: { display: false },
+  },
+  layout: {
+    padding: {
+      left: -7,
+      bottom: -7,
+    },
+  },
+}
+
+function plotLineChart(
+  parentElement,
+  data,
+  xKey,
+  yKeyRed,
+  yKeyBlue,
+  yKeyPurple,
+) {
+  document.querySelector(parentElement).innerHTML = ''
+  const timestamp = Date.now()
+  dom.addElement(parentElement, 'canvas', '', {
+    id: timestamp,
+    width: actualWidthCanvas,
+    height: actualHeightCanvas,
+    style: `width: ${scaledWidthCanvas}px; height: ${scaledHeightCanvas}px`,
+  })
+
+  new Chart(document.getElementById(timestamp), {
+    data: {
+      labels: data.map((row) => row[xKey]),
+      datasets: [
+        {
+          type: 'line',
+          data: data.map((row) => row[yKeyRed]),
+          borderColor: 'red',
+          borderWidth: 4,
+          tension: 0.5,
+          pointRadius: 0,
+        },
+        {
+          type: 'line',
+          data: data.map((row) => row[yKeyBlue]),
+          borderColor: 'blue',
+          borderWidth: 4,
+          tension: 0.5,
+          pointRadius: 0,
+        },
+        {
+          type: 'line',
+          data: data.map((row) => row[yKeyPurple]),
+          borderColor: 'purple',
+          borderWidth: 4,
+          tension: 0.5,
+          pointRadius: 0,
+        },
+      ],
+    },
+    options: chartOptions,
+  })
+}
+
+function plotBarChart(parentElement, data, xKey, yKey) {
+  document.querySelector(parentElement).innerHTML = ''
+  const timestamp = Date.now()
+  dom.addElement(parentElement, 'canvas', '', {
+    id: timestamp,
+    width: actualWidthCanvas,
+    height: actualHeightCanvas,
+    style: `width: ${scaledWidthCanvas}px; height: ${scaledHeightCanvas}px`,
+  })
+
+  new Chart(document.getElementById(timestamp), {
     data: {
       labels: data.map((row) => row[xKey]),
       datasets: [
@@ -50,60 +224,26 @@ function plotBarChart(data, xKey, yKey) {
         },
       ],
     },
-    options: {
-      scales: {
-        y: {
-          grid: { display: true },
-          min: 0, // Set minimum y value
-          max: 10, // Set maximum y value
-          ticks: { display: false },
-        },
-        x: { grid: { display: false }, ticks: { display: false } },
-      },
-      plugins: {
-        legend: { display: false },
-      },
-    },
+    options: chartOptions,
   })
 }
 
-function plotLineChart(data, xKey, yKey) {
-  new Chart(document.getElementById('temperature'), {
-    data: {
-      labels: data.map((row) => row[xKey]),
-      datasets: [
-        {
-          type: 'line',
-          data: data.map((row) => row[yKey]),
-          fill: false,
-          borderColor: 'rgba(75, 192, 192, 1)',
-          tension: 0.5, // flatten the curve 0-1
-          pointRadius: 0, // dots radius
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          grid: { display: false },
-          min: -15, // Set minimum y value
-          max: 15, // Set maximum y value
-          ticks: { display: false },
-        },
-        x: { grid: { display: false }, ticks: { display: false } },
-      },
-      plugins: {
-        legend: { display: false },
-      },
-    },
-  })
-}
+// search bar -----------------------------------------------------------------
 
-function plotIcons(data) {
-  data.forEach((icon) => {
-    dom.addElement('#icons', 'img', '', {
-      src: `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/refs/heads/main/PNG/2nd%20Set%20-%20Color/${icon}.png`,
-      style: 'width: 33px',
-    })
-  })
-}
+document.querySelector('input').addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    const searchInput = e.target
+    let searchTerm = searchInput.value
+    getWeatherData(searchTerm)
+    searchInput.value = ''
+    searchInput.blur()
+  }
+})
+
+document.querySelector('.searchIcon').addEventListener('click', () => {
+  const searchInput = document.querySelector('input')
+  let searchTerm = searchInput.value
+  getWeatherData(searchTerm)
+  searchInput.value = ''
+  searchInput.blur()
+})
